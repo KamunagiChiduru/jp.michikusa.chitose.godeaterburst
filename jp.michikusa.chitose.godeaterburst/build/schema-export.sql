2041a27c05197c2415f8407dc9b006c59b8b5dea
KamunagiChiduru <twilight_epitaph@hotmail.co.jp>
2013-01-15 08:01:37 +0900

    マッピング変更、generator.xmlのエラー回避

--- jp.michikusa.chitose.godeaterburst/build/schema-export.sql
+++ jp.michikusa.chitose.godeaterburst/build/schema-export.sql
@@ -1,4 +1,4 @@
+drop table if exists item_category;
 drop table if exists item_info;
-drop table if exists users;
-create table item_info (id  integer, category varchar not null, name varchar not null, nitems integer, buy_amount integer, sell_amount integer, note varchar, primary key (id));
-create table users (id  integer, name varchar, password varchar, primary key (id));
+create table item_category (id  integer, category varchar not null, category_id bigint not null, primary key (id));
+create table item_info (id  integer, category_id bigint not null, name varchar not null, nitems integer, buy_amount integer, sell_amount integer, note varchar, primary key (id));
