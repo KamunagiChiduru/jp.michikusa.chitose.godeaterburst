2f75d355fdf7407aa44c65c947c7275b234feb25
E.Sekito <sekito@bbreak.co.jp>
2013-01-15 19:59:15 +0900

    DBInitializerのテストで、insertとselectもしてみた

--- jp.michikusa.chitose.godeaterburst/test/jp/michikusa/chitose/setup/DatabaseInitializerTest.java
+++ jp.michikusa.chitose.godeaterburst/test/jp/michikusa/chitose/setup/DatabaseInitializerTest.java
@@ -1,14 +1,62 @@
 package jp.michikusa.chitose.setup;
 
-import static org.junit.Assert.assertTrue;
+import jp.michikusa.chitose.entity.ItemCategory;
+import jp.michikusa.chitose.util.H;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.assertNotNull;
+
+import static org.junit.Assert.assertTrue;
+
 public class DatabaseInitializerTest{
-	@Test
-	public void test() throws Exception{
+	@BeforeClass
+	public static void initDb() throws Exception{
 		DatabaseInitializer proc= new DatabaseInitializer();
 		
 		assertTrue(proc.call());
+	}
+	
+	@Test
+	public void insertAndSelect(){
+		Session s= H.instance().newSession();
+		
+		try{
+			Transaction tx= null;
+			try{
+				tx= s.beginTransaction();
+				
+				ItemCategory category= new ItemCategory();
+				
+				category.setName("消費アイテム");
+				
+				s.saveOrUpdate(category);
+				
+				tx.commit();
+			}
+			catch(Exception e){
+				if(tx != null){
+					tx.rollback();
+				}
+				throw new AssertionError(e);
+			}
+			
+			ItemCategory result= (ItemCategory)s
+					.createCriteria(ItemCategory.class)
+					.add(Restrictions.eq(ItemCategory.Column.name, "消費アイテム"))
+					.uniqueResult();
+			
+			assertNotNull(result);
+			assertEquals("消費アイテム", result.getName());
+		}
+		finally{
+			s.close();
+		}
 	}
 }
