4f528e46adc8ee63e9cf2704e39032985facc7af
E.Sekito <sekito@bbreak.co.jp>
2013-01-16 11:06:24 +0900

    プロジェクト固有の.gitignoreと、自動生成されるファイルは削除

--- jp.michikusa.chitose.godeaterburst/build/schema-export.sql
+++
@@ -1,4 +0,0 @@
-drop table if exists item_category;
-drop table if exists item_info;
-create table item_category (id  integer, name varchar not null, primary key (id), unique (name));
-create table item_info (id  integer, category_id bigint not null, name varchar not null, limit_numbers integer, purchasing_price integer, selling_price integer, note varchar, primary key (id), unique (category_id, name));
